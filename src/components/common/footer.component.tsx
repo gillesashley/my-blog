1bf0c8e680fbedd9c63066e32f6e2f7708ec9291
gillesashley <xylaray37@gmail.com>
2023-12-19 14:58:52 +0000

    Implement a basic footer, not catchy yet.

--- src/components/common/footer.component.tsx
+++ src/components/common/footer.component.tsx
@@ -1,7 +1,113 @@
+import {
+  Container,
+  Grid,
+  Typography,
+  Link,
+  Box,
+  Divider,
+  IconButton,
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
 export default function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer
+      style={{
+        backgroundColor: "#3f51b5",
+        padding: "2rem",
+        marginTop: "auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={3} justifyContent="space-between">
+          <Grid item xs={12} sm={6}>
+            <Typography
+              variant="h5"
+              color="textPrimary"
+              style={{ marginBottom: "2rem" }}
+            >
+              Your Logo or Site Name
+            </Typography>
+            <Typography variant="body1" color="textSecondary" gutterBottom>
+              Your tagline or a brief description of your site.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" color="textPrimary">
+                  Quick Links
+                </Typography>
+                <ul
+                  style={{
+                    listStyle: "none",
+                    padding: 0,
+                    display: "flex",
+                    flexWrap: "wrap",
+                    gap: "1rem",
+                  }}
+                >
+                  <li>
+                    <Link
+                      href="#"
+                      color="textPrimary"
+                      variant="body2"
+                      underline="hover"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      color="textPrimary"
+                      variant="body2"
+                      underline="hover"
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      color="textPrimary"
+                      variant="body2"
+                      underline="hover"
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" color="textPrimary">
+                  Connect
+                </Typography>
+                <Box style={{ display: "flex", gap: "2rem" }}>
+                  <IconButton color="inherit">
+                    <FacebookIcon />
+                  </IconButton>
+                  <IconButton color="inherit">
+                    <TwitterIcon />
+                  </IconButton>
+                  <IconButton color="inherit">
+                    <InstagramIcon />
+                  </IconButton>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Divider style={{ margin: "1.5rem 0" }} />
+        <Typography variant="body2" color="textSecondary" align="center">
+          Copyright © {new Date().getFullYear()} Your Company Name
+        </Typography>
+      </Container>
+    </footer>
   );
 }
