757c8fca3da01279ce0691b887c908a25ce03979
gillesashley <xylaray37@gmail.com>
2023-12-19 08:57:29 +0000

    Created and rendered browser router in main.tsx and created root.tsx as the root layout component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import Root from "./routes/root/root.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
